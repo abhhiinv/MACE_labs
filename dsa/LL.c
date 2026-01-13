7a96371dfb814581febd92ee0f5af99f98ff3790
Abhinav Sajeev <abhinavsajeev2004@gmail.com>
2026-01-13 10:35:14 +0530

    Refactor linked list operations and fix formatting

--- dsa/LL.c
+++ dsa/LL.c
@@ -1,101 +1,108 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct Node{
+typedef struct Node {
     int data;
-    struct node* next;
-}node;
+    struct Node* next;
+} node;
 
-node *head = NULL;
-node *temp = NULL;
+node* head = NULL;
+node* temp = NULL;
 
-node *create(int value){
-    node *new = (node*)malloc(sizeof(node));
-    new->data = value;
+node* createNode(int val){
+    node* new = (node*) malloc (sizeof(node));
+    new->data = val;
     new->next = NULL;
     return new;
 }
 
-void insertBeg(int value){
-    node *new = create(value);
-    if(head == NULL)
-        head=new;
-    else{
-        new->next=head;
-        head = new;
-    }
+void insert(int val){
+    node* new = createNode(val);
+    new->next = head;
+    head = new;
 }
 
-void insertEnd(int value){
-    node* new =create(value);
-    if(head == NULL)
+void insertEnd(int val){
+    node* new = createNode(val);
+    if(head == NULL){
         head = new;
-    else{
-        temp = head;
-        while(temp->next != NULL){
-            temp = temp->next;
-        }
-        temp->next = new;
-    }
-}
-
-void deleteBeg(){
-    if(head == NULL)
         return;
-    else{
-        temp = head;
-        temp->next = head;
     }
-    printf("%d deleted",temp->data);
+    temp = head;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = new;
+}
+
+void delete(){
+    if(head == NULL) return;
+    temp = head;
+    head = head->next;
+    printf("%d deleted\n",temp->data);
     free(temp);
 }
 
-void delteEnd(){
-    node *prev = NULL;
-    if(head == NULL)
+void deleteEnd(){
+    if(head==NULL) return;
+    if(head->next == NULL){
+        printf("%d deleted",head->data);
+        free(head);
+        head = NULL;
         return;
-    else{
-        temp = head;
-        while(temp->next != NULL){
-            prev = temp;
-            temp = temp->next;
-        }
     }
-    printf("%d deleted",temp->data);
-    free(temp);
+    temp = head;
+    while(temp->next->next != NULL){
+        temp = temp->next;
+    }
+    printf("%d deleted",temp->next->data);
+    free(temp->next);
+    temp->next = NULL;
 }
 
 void display(){
+    if(head == NULL){
+        printf("Nothing to display\n");
+        return;
+    }
     temp = head;
-    while(temp->next != NULL){
+    while(temp != NULL){
         printf("%d->",temp->data);
         temp = temp->next;
     }
-    printf("%d->NULL\n",temp->data);
+    printf("NULL\n");
 }
 
-void main(){
+int main(){
     int ch,val;
     while(1){
-        printf("1)Insert Begin\n2)Insert End\n3)Delete Begin\n4)Delete End\n5)Display\n6)Exit\n");
+        printf("\n1)Insertion at beginning\n2)Deletion from beiginning\n3)Insert from end\n4)Delete from end\n5)Display\n6)Exit");
+        printf("Enter your choice : ");
         scanf("%d",&ch);
-        switch (ch)
-        {
-        case 1 :
-            printf("Enter value : ");
-            scanf("%d",&val);
-            insertBeg(val);
-            break;
-        case 2:
-            printf("Enter value : ");
-            scanf("%d",&val);
-            insertEnd(val);
-            break;
-        case 3:
-            
-            delete
-        default:
-            break;
+        switch(ch){
+            case 1:
+                printf("Enter value to insert from beginning: ");
+                scanf("%d",&val);
+                insert(val);
+                break;
+            case 2:
+                delete();
+                break;
+            case 3:
+                printf("Enter value to insert at end: ");
+                scanf("%d",&val);
+                insertEnd(val);
+                break;
+            case 4: 
+                deleteEnd();
+            case 5:
+                display();
+                break;
+            case 6:
+                exit(0);
+            default:
+                printf("Invalid choice\n");
         }
     }
+    return 0;
 }
