b5c358887c304fab1ff3c3e1726606268ae4289c
Abhinav Sajeev <abhinavsajeev2004@gmail.com>
2026-02-06 15:38:40 +0530

    Add files via upload

--- SEM-2/ADBMS/DCL.sql
+++ SEM-2/ADBMS/DCL.sql
@@ -12,7 +12,23 @@ SELECT * FROM dba_tab_privs WHERE grantee = 'ABHINAV';
 -- Connect as abhinav
 CONNECT abhinav/123
 
-SELECT * FROM Employee;
-INSERT INTO system.Employee VALUES (201, 'Abhinav', 55000.00);
-UPDATE system.Employee SET Salary = 60000.00 WHERE ID = 201;
-DELETE FROM system.Employee WHERE ID = 201;
+SELECT * FROM sys.Employee;
+INSERT INTO sys.Employee VALUES (201, 'Abhinav', 55000.00);
+UPDATE sys.Employee SET Salary = 60000.00 WHERE ID = 201;
+DELETE FROM sys.Employee WHERE ID = 201;
+
+grant dba to abhinav;
+
+create user user2 identified by 123;
+
+grant create session to user2;
+GRANT SELECT, INSERT ON sys.Employee TO user2;
+
+select * from sys.Employee;
+insert into sys.Employee values(201,'Abhinav',55000.00);
+update sys.Employee set Salary = 60000.00 where ID = 201;
+
+REVOKE SELECT, INSERT ON sys.Employee FROM user2;
+revoke dba from user2;
+
+drop user user2;
