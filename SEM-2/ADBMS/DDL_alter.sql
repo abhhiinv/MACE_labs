47750a4ef896a76fce8add462f5f8df557e9136a
Abhinav Sajeev <abhinavsajeev2004@gmail.com>
2026-02-13 15:41:40 +0530

    Add files via upload

--- SEM-2/ADBMS/DDL_alter.sql
+++ SEM-2/ADBMS/DDL_alter.sql
@@ -2,3 +2,6 @@ alter table Student_1 add Sem int;
 alter table Student_1 rename column Sem to Semester;
 alter table Student_1 drop column Year;
 alter table Student_1 modify Name varchar(100);
+UPDATE Sailors SET age=55.5 WHERE sid = 31;
+UPDATE Sailors SET age=33.0 WHERE sid = 29;
+UPDATE Sailors SET age=45.0 WHERE sid = 22;
