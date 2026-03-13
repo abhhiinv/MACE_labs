1ff2da1d56a91de35aa6d248c42c970c80451d09
Abhinav Sajeev <abhinavsajeev2004@gmail.com>
2026-03-13 10:56:47 +0530

    updated code

--- SEM-2/OOPS/arithmeticDemo.java
+++ SEM-2/OOPS/arithmeticDemo.java
@@ -1,5 +1,15 @@
+import arithmetic.*;
+
 public class arithmeticDemo {
     public static void main(String[] args) {
-        
+        Operation add = new Add();
+        Operation subtract = new Subtract();
+        Operation multiply = new Multiply();
+        Operation divide = new Divide();
+
+        System.out.println("2 + 3 = " + add.calculate(2, 3));
+        System.out.println("5 - 2 = " + subtract.calculate(5, 2));
+        System.out.println("4 * 3 = " + multiply.calculate(4, 3));
+        System.out.println("10 / 2 = " + divide.calculate(10, 2));
     }
 }
